12b3de381923305940c76886336081483500cee4
yenslife <77geo5rge6@gmail.com>
2024-01-28 19:33:40 +0800

    feat: 新增 footer

---
+++ src/components/Footer/index.js
@@ -0,0 +1,49 @@
+import React from "react";
+import styled from "styled-components";
+
+// Create styled components
+const FooterContainer = styled.footer`
+    background-color: rgb(172, 172, 172);
+    display: flex;
+    align-items: center;
+    padding: 10px;
+    bottom: 0;
+    font-family: Arial, Helvetica, sans-serif;
+`;
+
+const FooterLogo = styled.img`
+    margin-right: 10px;
+`;
+
+const FooterContent = styled.div`
+    display: flex;
+    flex-direction: column;
+`;
+
+const FooterInfo = styled.p`
+    margin-bottom: 5px;
+`;
+
+const Footer = () => {
+    return <div>
+        <FooterContainer>
+            <FooterLogo src="https://cdn-icons-png.flaticon.com/128/3698/3698586.png" alt="Logo" id="footer-logo" />
+            <FooterContent>
+                <FooterInfo>About us : <a
+                    href="https://ismp.csie.ncku.edu.tw/"
+                    target="_blank"
+                    style={{ color: "black" }}
+                    rel="noopener noreferrer"
+                >
+                    成大智慧型系統暨媒體處理實驗室
+                </a>
+                </FooterInfo>
+                <FooterInfo>Authors : Yenslife , Horseface , Exn , Booker , 江忠益 , 洪盟峰 , 郭耀煌</FooterInfo>
+                <FooterInfo>Designers : 黃淑俐 , Xiang-Yi</FooterInfo>
+                <FooterInfo>Contact us : caladmin@ismp.csie.ncku.edu.tw</FooterInfo>
+            </FooterContent>
+        </FooterContainer>
+    </div>;
+}
+
+export default Footer;
