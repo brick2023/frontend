61ec4f51ba2402b80d0c9275233d5f998d3453bf
yenslife <77geo5rge6@gmail.com>
2024-01-28 19:33:58 +0800

    refactor: 將 HomePage 的 Navbar 獨立成一個檔案
    修正項目與原因：
    這個修正可以讓 HomePage 更簡潔，考慮到之後 HomePage 會有複雜的程式碼
    所以先外移他們了，現在 HomePage.js 只有基本的元件 Navbar, Footer, features
    其中 Features 是在 HomePage 資料夾底下的檔案，因為只有 HomePage 會用到
    feature (登入之前看到的畫面)。

--- src/components/LoginModal/index.js
+++ src/components/LoginModal/index.js
@@ -1,22 +1,60 @@
 import React from "react";
+import { useState, useEffect } from "react";
 import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
+import api from "api/account";
 
 const LoginModal = ({ show, onClose }) => {
+
+    const [account, setAccount] = useState('');
+    const [password, setPassword] = useState('');
+    const [loginStatus, setLoginStatus] = useState('');
+    const [jwt, setJwt] = useState(''); // jwt token
+
+    useEffect(() => {
+        if (jwt) {
+            localStorage.setItem('jwt', jwt);
+            console.log(jwt);
+        }
+    }, [jwt]); // 只有當 jwt 改變時才會觸發
+
+
+    const handleLogin = async () => {
+        const res = await api.post('/login', {
+            email: account,
+            password,
+        });
+
+        const loginMessage = res.data.message;
+        console.log(loginMessage);
+
+        // set login status if 0 then login success, 1 no account, 2 password error
+        if (loginMessage === 0) {
+            setLoginStatus('登入成功');
+            setJwt(res.data.jwt.access_token);
+            localStorage.setItem('jwt', jwt);
+        } else if (loginMessage === 1) {
+            setLoginStatus('無此帳號');
+        } else if (loginMessage === 2) {
+            setLoginStatus('密碼錯誤');
+        }
+    }
+
     return (
         <Modal show={show} onHide={onClose}>
             <Modal.Header closeButton>
                 <Modal.Title>登入</Modal.Title>
             </Modal.Header>
             <Modal.Body>
-                <input type="text" placeholder="帳號" />
-                <input type="password" placeholder="密碼" />
+                <input type="text" placeholder="帳號" value={account} onChange={(e) => setAccount(e.target.value)} />
+                <input type="password" placeholder="密碼" value={password} onChange={(e) => setPassword(e.target.value)} />
+                <p>登入狀態：{loginStatus}</p>
             </Modal.Body>
             <Modal.Footer>
                 <Button variant="secondary" onClick={onClose}>
                     關閉
                 </Button>
-                <Button variant="primary">
+                <Button variant="primary" onClick={handleLogin}>
                     登入
                 </Button>
             </Modal.Footer>
