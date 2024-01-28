4e1bd723df510563f8aee73554efde8c31b45c19
yenslife <77geo5rge6@gmail.com>
2024-01-28 19:31:56 +0800

    refactor: 修改 account.js 的 export 方式(這樣才能用)

--- src/api/account.js
+++ src/api/account.js
@@ -5,5 +5,4 @@ const accountApi = axios.create({
     baseURL: 'http://localhost:8000/account',
 })
 
-export const login = () => accountApi.post('/login')
-export const register = () => accountApi.post('/register')
+export default accountApi;
