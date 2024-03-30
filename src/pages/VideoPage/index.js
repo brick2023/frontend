1974382f3177cad2f0607aba32f61008b8ddf128
yenslife <77geo5rge6@gmail.com>
2024-03-30 13:04:32 +0800

    新增從後端拿影片的方法
    在 VideoPage 中使用 fetch 的方式
    和後端要影片

--- src/pages/VideoPage/index.js
+++ src/pages/VideoPage/index.js
@@ -1,13 +1,31 @@
 import React from "react";
 import ReactPlayer from "react-player";
+import { useEffect } from "react";
 
 const VideoPage = () => {
+    
+    const [videoUrl, setVideoUrl] = React.useState('');
+
+    useEffect(() => {
+        fetch('http://localhost:8000/user/test')
+            .then(response => response.blob ())
+            .then(blob => {
+                const url = URL.createObjectURL(blob);
+                setVideoUrl(url);
+            })
+            .catch((error) => {
+                console.error('Error fetching or processing video:', error);
+            }
+        );
+    }, []);
+
     return (
         <div>
             <h1 style={{marginBottom: "20px", marginTop: "20px", textAlign: "center"}}>Lesson Name</h1>
             <div style={{ display: "flex", justifyContent: "center", alignItems: "center"}}>
                 <ReactPlayer 
-                    url="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+                    // url="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+                    url = {videoUrl}
                     playing={true}
                     controls={true}
                 />
