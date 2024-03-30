376a608bfd817ab775956d1386505df7f01b0af5
yenslife <77geo5rge6@gmail.com>
2024-03-30 12:14:59 +0800

    將 strict mode 弄回來
    因為沒有弄回來就看不到 console.log
    會跳出一堆警告訊息

--- src/index.js
+++ src/index.js
@@ -1,21 +1,22 @@
 import React from 'react';
-//import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-/*
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
+  // 之後要部署的時候，把strict mode拿掉
 );
-*/
+
 
 // Use this version instead of the version above is to solve react-simple-chatbot problem about message duplication
-import { render } from "react-dom";
-render(<App />, document.getElementById("root"));
+// import { render } from "react-dom";
+// render(<App />, document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
