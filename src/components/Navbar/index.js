86c27f9ed144dced5febf3790b2becf64f5dad4d
Booker-Chen <booker0216@gmail.com>
2024-04-04 23:50:38 +0800

    Style the navbar and add sidebar.
    Create a nav.css to style the navbar and add a search function on
    the navbar. Using react-pro-sidebar to create a course sidebar.

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,17 +1,32 @@
 import React from "react";
 import { useState } from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './nav.css';
 import LoginModal from "components/LoginModal";
 
 const Navbar = ({setIsLogin}) => {
     const [showLoginModal, toggleLoginModal] = useModal(false);
+
+    const search = (formData) => {
+        const query = formData.get('query');
+        alert(`Search: ${query}`);
+    }
+
     return <div>
         <nav className="navbar sticky-top bg-body-tertiary navbar-design">
             <div className="container-fluid">
-                <div className="navbar-llama-relative-box">
+                <div className="navbar-brand">
                     <img src="https://cdn-icons-png.flaticon.com/128/3698/3698586.png" alt="Logo" className="navbar-llama" />
+                    <p className="navbar-title">NCKU Self-Learning</p>
                 </div>
-                <p className="navbar-title">NCKU Self-Learning</p>
+
+                <form action={search}>
+                    <input name="query" placeholder="Search" className="search-form-design" />
+                    <button type="submit" className="search-button">
+                        <img className="search-icon" src="https://cdn-icons-png.flaticon.com/512/10385/10385257.png" alt="Search" />
+                    </button>
+                </form>
+
                 <button type="button" onClick={toggleLoginModal}>
                     Log In
                 </button>
--- src/pages/HomePage/home.js
+++ src/pages/HomePage/home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import { Sidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar';
 import Chatbot from './chatbot';
 
 const Home = () => {
@@ -10,13 +11,34 @@ const Home = () => {
     };
 
     return (
-        <div>
+        <div style={{ display: 'flex', minHeight: '85vh'}}>
+          <Sidebar backgroundColor="#FFF1C0">
+            <Menu>
+              <SubMenu label="Course 1">
+                <MenuItem> Lesson 1 </MenuItem>
+                <MenuItem> Lesson 2 </MenuItem>
+                <MenuItem> Lesson 3 </MenuItem>
+              </SubMenu>
+              <SubMenu label="Course 2">
+                <MenuItem> Lesson 1 </MenuItem>
+                <MenuItem> Lesson 2 </MenuItem>
+              </SubMenu>
+              <SubMenu label="Course 3">
+                <MenuItem> Lesson 1 </MenuItem>
+                <MenuItem> Lesson 2 </MenuItem>
+                <MenuItem> Lesson 3 </MenuItem>
+                <MenuItem> Lesson 4 </MenuItem>
+              </SubMenu>
+            </Menu>
+          </Sidebar>
+          <main style={{ padding: 10 }}> 
             <div className='Courses'>
                 <img src="https://thumb.ac-illust.com/35/35f61f14dbe475df743fa2955a49ee3d_t.jpeg" alt="Video" onClick={handleImageClick} />
                 <img src="https://thumb.ac-illust.com/35/35f61f14dbe475df743fa2955a49ee3d_t.jpeg" alt="Video" onClick={handleImageClick} />
                 <img src="https://thumb.ac-illust.com/35/35f61f14dbe475df743fa2955a49ee3d_t.jpeg" alt="Video" onClick={handleImageClick} />
             </div>
             <Chatbot />
+          </main>
         </div>
     );
 };
