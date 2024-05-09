f9e197ac427eba1b5741d9c4ec9379d785e3b8b7
horseface1110 <oloomb6@gmail.com>
2024-05-09 02:50:48 -0400

    完成了前端影片名稱，後端時間記錄創建、刪除、前段摘要，主要是放棄timestamp改用time形事，目前全部的api都可以動了 影片開始時間已經可以正確傳送了，但不知道未甚麼影片沒有照我想要的跑起來

--- src/pages/VideoPage/index.js
+++ src/pages/VideoPage/index.js
@@ -3,17 +3,22 @@ import ReactPlayer from "react-player";
 import { useEffect, useState } from "react";
 // import user api test to get video
 import { getTest } from "api/user";
+import { getVideo, getName, getTime } from "api/video";
+
+const video_id = 15;
 
 const VideoPage = () => {
     
     const [videoUrl, setVideoUrl] = useState('');
+    const [videoName, setVideoName] = useState('');
+    const [videoTime, setVideoTime] = useState('');
 
     useEffect(() => {
         // async get Test video
         async function fetchVideo() {
             try {
                 console.log('fetching video');
-                const response = await getTest();
+                const response = await getVideo(video_id);
                 const url = URL.createObjectURL(response.data);
                 setVideoUrl(url);
                 console.log(url);
@@ -21,20 +26,45 @@ const VideoPage = () => {
                 console.error('Error fetching or processing video:', error);
             }
         }
+        const fetchVideoName = async () => {
+            try{
+                console.log('fetch video name');
+                const videoName  = await getName(video_id);
+                const name = videoName.data;
+                console.log('name:', name);
+                setVideoName(name);
+            } catch (error) {
+                console.error('Error fetching or processing video:', error);
+            }
+        }
+        const fetchVideoTime = async() => {
+            try {
+                console.log('fetch vidoe time')
+                const Time  = await getTime(video_id);
+                const videoTime = Time.data;
+                const [hours, minutes, seconds] = videoTime.split(':').map(parseFloat);
+                const total = hours * 3600 + minutes * 60 + seconds;
+                console.log('Time',total);
+                setVideoTime(total);
+            } catch (error) {
+                console.error('Error fetching or processing video:', error);
+            }
+        }
         fetchVideo();
+        fetchVideoName();
+        fetchVideoTime();
     }, []);
 
     return (
         <div>
-            <h1 style={{marginBottom: "20px", marginTop: "20px", textAlign: "center"}}>Lesson Name</h1>
+            <h1 style={{marginBottom: "20px", marginTop: "20px", textAlign: "center"}}>Lesson Name:{videoName}</h1>
             <div style={{ display: "flex", justifyContent: "center", alignItems: "center"}}>
                 <ReactPlayer 
-                    // url="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
                     url = {videoUrl}
-                    // url = "http://brick2.yenslife.top:2023/user/test"
                     playing={true}
                     controls={true}
                     preload="auto"
+                    startTime={videoTime}
                 />
             </div>
         </div>
